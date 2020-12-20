4a7d5a51108008069db80c4d8d6b2207ca8f9444
amineb01 <bellarej.amine@gmail.com>
2020-12-20 19:59:29 +0100

    add angular service to docker compose

--- frontend/src/helpers/token-interceptor.ts
+++ frontend/src/helpers/token-interceptor.ts
@@ -4,13 +4,15 @@ import { Observable } from 'rxjs';
 
 import { AuthService } from '../services/auth.service';
 
+import { environment } from '../environments/environment';
+
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
     constructor(private authenticationService: AuthService) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
-        request = request.clone({ url: `http://127.0.0.1:3000/${request.url}` });
+        request = request.clone({ url: `${environment.apiUrl}/${request.url}` });
 
         let currentUser = this.authenticationService.currentUserValue;
         if (currentUser && currentUser['token']) {
