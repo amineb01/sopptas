4fc813fbd212ae65226053b12ae9e7f7dc62c72c
amineb01 <bellarej.amine@gmail.com>
2021-01-07 22:19:25 +0100

    change backend url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://0.0.0.0/api/',
-  assetsUrl: 'http://0.0.0.0/images/'
+  apiUrl: 'http://41.231.36.75/api/',
+  assetsUrl: 'http://41.231.36.75/images/'
 
 };
