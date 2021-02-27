fc217b9e80ce4dd66c03c03f41ebc80d523ab08f
seifeddin <seif1994@yahoo.fr>
2021-02-27 22:06:55 +0100

    fix url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/',
-  assetsUrl: 'http://localhost:3000/images/'
+  apiUrl: 'http://0.0.0.0/api/',
+  assetsUrl: 'http://0.0.0.0/images/'
 
 };
