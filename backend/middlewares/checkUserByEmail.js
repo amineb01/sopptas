18f01baf360f9facee6377516e5ec433970a1191
seifeddin <seif1994@yahoo.fr>
2021-04-19 13:59:06 +0100

    FIX-login-with-device-token

--- backend/middlewares/checkUserByEmail.js
+++ backend/middlewares/checkUserByEmail.js
@@ -8,13 +8,16 @@ const checkUserByEmail = ( req, res ) => {
   User.findOne({
       email: req.body.email
     })
-    .select('email name password role')
+    .select('email name password role device_token')
     .exec()
     .then(user => {
       if(!user){
         deferred.reject("user not found");
       }else{
         console.log(user)
+        User.findOneAndUpdate({email: req.body.email },{device_token: req.body.device_token},{new:true})
+        .then(res =>{
+        })
         req.body.cryptedPassword = user.password
         req.body.id = user._id
         req.body.role = user.role
